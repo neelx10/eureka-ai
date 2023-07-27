cf47c2b75ab428d2f38e99ee4879007db65ca0a8
Neelotpal <neelotpal10@gmail.com>
2023-07-27 18:51:16 +0530

    second

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,10 @@
-//import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+// //import { Button } from "@/components/ui/button";
+// import { UserButton } from "@clerk/nextjs";
 
 const DashBoardPage = () => {
   return (
     <div>
       <p>DashBoard Page (Protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
